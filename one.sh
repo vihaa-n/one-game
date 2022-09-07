22e49274969b1b12e385f4a2e7ec9780f40cd63e
vihaa-n <108354079+vihaa-n@users.noreply.github.com>
2022-09-07 04:54:39 +0300

    Add files via upload

---
+++ one.sh
@@ -0,0 +1,17 @@
+echo "The Game of the One"
+sleep 1
+read "Thoust Code(2 sentence-)?" abc
+sleep 1 
+if [$abc == "Harry Potter"] 
+then 
+    echo $abc > a.txt 
+    vs = awk {$0} a.txt
+    echo "$vs : is the key. Use Wisely."
+
+else
+    echo "Back off, imposter! Thou art a spy!"
+    exit 
+fi
+
+echo "Now, go to the High Point, and say $vs! "
+
